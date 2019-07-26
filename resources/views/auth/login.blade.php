2a4dd360f2d1a9de99637dd03b9cf2274965b860
Bruno Bueno <b.buenoo94@gmail.com>
2019-07-26 17:33:17 -0300

    Login com facebbok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,6 +65,13 @@
                             </div>
                         </div>
                     </form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="/auth/facebook">
+                            <img src="{{asset('img/facebook-logo.png') }}" alt="" srcset="">
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
